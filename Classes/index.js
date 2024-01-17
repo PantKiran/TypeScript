e276de08c5dc62dc2d136e7dae90b725631b80b1
kiran pant <kiran.r.pant@gmail.com>
2024-01-17 17:37:17 +0545

    initial commit

--- Classes/index.js
+++ Classes/index.js
@@ -1,4 +1,5 @@
 class Person {
+  static description = "static is for Class and not for particular instances";
   #initialBalance = 0;
   currentBalance = 757;
   constructor(name, age) {
@@ -27,9 +28,23 @@ class Person {
   // }
 }
 
+// extending Class
+
+class AdminPerosn extends Person {
+  isAdmin = true;
+  constructor(name, age, permissions) {
+    super(name, age);
+    this.permissions = permissions;
+  }
+}
+
+const admin = new AdminPerosn("kiran", 27, ["update", "delete"]);
+console.log(admin);
+
 const Kiran = new Person("kiran", 27);
 console.log(Kiran.initialBalance);
-console.log((Kiran.initialBalance = -20));
+console.log((Kiran.initialBalance = 20));
+console.log(Person.description);
 // console.log(Kiran.getInitalBlance());
 // console.log(Kiran.setInitialBalance(30));
 // console.log(Kiran.Name);
