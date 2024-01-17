9eec9c796b8332131c8e77f5a26aff90b420a2b2
kiran pant <kiran.r.pant@gmail.com>
2024-01-17 16:39:32 +0545

    initial commit

--- Classes/index.js
+++ Classes/index.js
@@ -7,17 +7,30 @@ class Person {
   greet() {
     console.log(`Hello ${this.name} you are ${this.age} years old`);
   }
+  get initialBalance() {
+    // getter methos
+    return `Hello ${this.name}`;
+  }
 
-  getInitalBlance() {
-    return this.#initialBalance;
+  set initialBalance(newBalance) {
+    if (newBalance < 0) {
+      throw new Error("Score must be positive");
+    }
+    this.#initialBalance = newBalance;
   }
-  setInitialBalance(newBalance) {
-    return (this.#initialBalance = newBalance);
-  }
+
+  // getInitalBlance() {
+  //   return this.#initialBalance;
+  // }
+  // setInitialBalance(newBalance) {
+  //   return (this.#initialBalance = newBalance);
+  // }
 }
 
 const Kiran = new Person("kiran", 27);
-console.log(Kiran.getInitalBlance());
-console.log(Kiran.setInitialBalance(30));
-
-Kiran.greet();
+console.log(Kiran.initialBalance);
+console.log((Kiran.initialBalance = -20));
+// console.log(Kiran.getInitalBlance());
+// console.log(Kiran.setInitialBalance(30));
+// console.log(Kiran.Name);
+// Kiran.greet();
